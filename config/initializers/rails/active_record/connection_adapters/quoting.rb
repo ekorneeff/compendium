913fdcd599a6a8389ac122a6bf7f3d8d8e60f8e8
Eugene Korneev <ekorneeff@gmail.com>
2017-04-13 16:15:02 +0300

    Disable AR relatives

--- config/initializers/rails/active_record/connection_adapters/quoting.rb
+++ config/initializers/rails/active_record/connection_adapters/quoting.rb
@@ -3,12 +3,12 @@
 # crash.
 # Override AR::ConnectionAdapters::Quoting to forward a SimpleDelegator's object to be quoted.
 
-module ActiveRecord::ConnectionAdapters::Quoting
-  def quote_with_simple_delegator(value, column = nil)
-    return value.quoted_id if value.respond_to?(:quoted_id)
-    value = value.__getobj__ if value.is_a?(SimpleDelegator)
-    quote_without_simple_delegator(value, column)
-  end
+# module ActiveRecord::ConnectionAdapters::Quoting
+#   def quote_with_simple_delegator(value, column = nil)
+#     return value.quoted_id if value.respond_to?(:quoted_id)
+#     value = value.__getobj__ if value.is_a?(SimpleDelegator)
+#     quote_without_simple_delegator(value, column)
+#   end
 
-  alias_method_chain :quote, :simple_delegator
-end
+#   alias_method_chain :quote, :simple_delegator
+# end
